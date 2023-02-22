c66bf24ee5023d620dabf55961508d30bbc6aadb
Muryllo <muryllo.dev@gmail.com>
2023-02-22 00:44:11 -0300

    add company creation layout

--- src/pages/Company/CompanyCreation/index.tsx
+++ src/pages/Company/CompanyCreation/index.tsx
@@ -1,3 +1,40 @@
+import { Breadcrumb, Button, Form, Input } from "antd";
+import { Link } from "react-router-dom";
+import { routePaths } from "../../../routes";
+import "./index.css";
+
 export const CompanyCreation = () => {
-  return <h1>Company Creation</h1>;
+  return (
+    <>
+      <Breadcrumb>
+        <Breadcrumb.Item>
+          <Link to={routePaths.company.list}>Empresas</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>Criar</Breadcrumb.Item>
+      </Breadcrumb>
+      <Form
+        onFinish={(values) => {
+          console.log(values);
+        }}
+        className="company-form"
+      >
+        <Form.Item
+          label="Nome"
+          name="name"
+          rules={[{ required: true, message: "Campo obrigatório" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="company-form-button"
+          >
+            Salvar
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
 };
